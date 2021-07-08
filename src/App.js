eb625a20cb68afe20d315470fe889884a92cc1f9
Rokas Giedrys <giedrysrokas@gmail.com>
2021-07-08 19:40:19 +0100

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route, Link } from "react-router-dom";
+
+const hats = () => {
+  return (
+    <div>
+      <h1>hats</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={hats} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,30 +14,35 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           size: "",
+          linkUrl: "hats",
         },
         {
           id: 2,
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           size: "",
+          linkUrl: "jackets",
         },
         {
           id: 3,
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/trousers.png",
           size: "",
+          linkUrl: "sneakers",
         },
         {
           id: 4,
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
+          linkUrl: "mens",
         },
         {
           id: 5,
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
+          linkUrl: "womens",
         },
       ],
     };
@@ -46,8 +51,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
