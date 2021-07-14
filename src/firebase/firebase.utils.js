ddc2168cd1e85d78e92b28179057ec6d857d324e
Rokas Giedrys <giedrysrokas@gmail.com>
2021-07-14 19:47:04 +0100

    Google auth added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyC9FudO53Y7QqAEM1jYRxSh0a4YyHmijYk",
+  authDomain: "cw-clothing-5662b.firebaseapp.com",
+  projectId: "cw-clothing-5662b",
+  storageBucket: "cw-clothing-5662b.appspot.com",
+  messagingSenderId: "13275192960",
+  appId: "1:13275192960:web:b1fb46a4c44de92a63d065",
+  measurementId: "G-EPLYTMYNVR",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
